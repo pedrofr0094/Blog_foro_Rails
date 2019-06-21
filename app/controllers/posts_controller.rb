7794c4a82bbed602f2cf41dcf4536f737a2b5dab
pedrofr0094 <pedrofr0094@gmail.com>
2019-06-21 12:26:03 +0200

    añadir post actions and views; new, create, update, destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -18,6 +21,30 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
   private
